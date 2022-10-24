063087affe7e5f3af637da1b0a27fe297042b15a
Sourav Paul <35699196+SouravPall@users.noreply.github.com>
2022-10-24 15:06:04 +0600

    button click count

--- MyButtonDemo/app/src/main/java/com/paul/mybuttondemo/MainActivity.java
+++ MyButtonDemo/app/src/main/java/com/paul/mybuttondemo/MainActivity.java
@@ -3,12 +3,33 @@ package com.paul.mybuttondemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int count = 0;
+    private Button loginButton;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.loginBtnID);
+        textView = findViewById(R.id.textViewID);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                textView.setText("LogIn button is Clicked "+ count+" times");
+
+            }
+        });
+
+
     }
 }
