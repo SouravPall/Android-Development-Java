ce5a1aa7ae128b191cce27010bace8ab5f8a3fc0
Sourav Paul <35699196+SouravPall@users.noreply.github.com>
2022-10-24 11:47:28 +0600

    find text view

--- TextViewDemo/app/src/main/java/com/paul/textviewdemo/MainActivity.java
+++ TextViewDemo/app/src/main/java/com/paul/textviewdemo/MainActivity.java
@@ -3,12 +3,27 @@ package com.paul.textviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView nameTextView;
+    private TextView nameTextView2;
+    private TextView ageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameTextView = findViewById(R.id.nameTextViewID);
+        nameTextView.setText("Sonjoy Paul");
+
+        nameTextView2 = findViewById(R.id.nickNameTextViewID);
+        nameTextView2.setText("(Sourav Paul)");
+
+        ageTextView = findViewById(R.id.ageTextViewID);
+        ageTextView.setText("25+ years old");
+
     }
 }
