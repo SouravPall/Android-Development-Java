4a5afacc02a158e3c5aa6bfb8a6bd8ff884bd94e
Sourav Paul <35699196+SouravPall@users.noreply.github.com>
2022-10-25 17:32:08 +0600

    input calculation done

--- MyEditText/app/src/main/java/com/paul/myedittext/MainActivity.java
+++ MyEditText/app/src/main/java/com/paul/myedittext/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -42,28 +43,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
-       String number1 = editText1.getText().toString();
-       String number2 = editText2.getText().toString();
+       try {
+           String number1 = editText1.getText().toString();
+           String number2 = editText2.getText().toString();
 
-       //converting into double
-        double num1 = Double.parseDouble(number1);
-        double num2 = Double.parseDouble(number2);
+           //converting into double
+           double num1 = Double.parseDouble(number1);
+           double num2 = Double.parseDouble(number2);
 
-        if (v.getId()==R.id.addButtonId){
+           if (v.getId()==R.id.addButtonId){
 
-            double sum = num1 + num2;
-            resultTextView.setText("Result : " + sum);
-        }else if (v.getId() == R.id.subButtonId){
-            double sub = num1-num2;
-            resultTextView.setText("Result : "+sub);
-        }else if (v.getId() == R.id.productButtonId){
-            double product = num1 * num2;
-            resultTextView.setText("Result: "+product);
-        }else {
-            double division = num1 / num2;
-            resultTextView.setText("Result: "+division);
-        }
+               double sum = num1 + num2;
+               resultTextView.setText("Result : " + sum);
+           }else if (v.getId() == R.id.subButtonId){
+               double sub = num1-num2;
+               resultTextView.setText("Result : "+sub);
+           }else if (v.getId() == R.id.productButtonId){
+               double product = num1 * num2;
+               resultTextView.setText("Result: "+product);
+           }else {
+               double division = num1 / num2;
+               resultTextView.setText("Result: "+division);
+           }
 
+       }catch (Exception e){
+           Toast.makeText(this, "Please input numbers", Toast.LENGTH_SHORT).show();
+       }
 
     }
 }
