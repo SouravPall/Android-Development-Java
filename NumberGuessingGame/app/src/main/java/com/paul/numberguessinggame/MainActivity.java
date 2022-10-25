16fd801b8c9b99ba9bbde5e0624323998d997810
Sourav Paul <35699196+SouravPall@users.noreply.github.com>
2022-10-25 19:08:25 +0600

    untill debugging

---
+++ NumberGuessingGame/app/src/main/java/com/paul/numberguessinggame/MainActivity.java
@@ -0,0 +1,64 @@
+package com.paul.numberguessinggame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private EditText editText;
+    private Button checkButton;
+    private TextView resultTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        editText = findViewById(R.id.editTextId);
+        checkButton = findViewById(R.id.checkButtonId);
+        resultTextView = findViewById(R.id.resultTextViewId);
+
+
+        checkButton.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+        String guessString = editText.getText().toString();
+
+        if (guessString.isEmpty()){
+            
+
+        }else{
+            int guess = Integer.parseInt(guessString);
+
+
+            //Generation random number
+            Random random = new Random();
+            int randomNumber = random.nextInt(5)+1;
+
+            if (randomNumber == guess){
+                resultTextView.setText("Congrats!! You have Won");
+            }else {
+                resultTextView.setText("Sorry! You have Lost. Random Number was "+ randomNumber);
+            }
+        }
+
+
+
+    }
+}
