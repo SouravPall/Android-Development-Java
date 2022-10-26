af71561ba1c7be099b23f42ddbe6e5433249f971
Sourav Paul <35699196+SouravPall@users.noreply.github.com>
2022-10-26 15:24:37 +0600

    uap-cse-webview app

---
+++ WebsiteConvert/app/src/main/java/com/paul/websiteconvert/MainActivity.java
@@ -0,0 +1,28 @@
+package com.paul.websiteconvert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView_id);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://cse.uap-bd.edu/");
+    }
+}
