101b6624b47ab28302dffb1c19766dd5f42efa5c
Sourav Paul <35699196+SouravPall@users.noreply.github.com>
2022-10-30 13:13:25 +0600

    set on click on country name

--- ListViewDemo/app/src/main/java/com/paul/listviewdemo/MainActivity.java
+++ ListViewDemo/app/src/main/java/com/paul/listviewdemo/MainActivity.java
@@ -3,8 +3,11 @@ package com.paul.listviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,R.layout.sample_view,R.id.textView_id, countryName);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String value = countryName[position];
+
+                Toast.makeText(MainActivity.this, value + " "+position, Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
