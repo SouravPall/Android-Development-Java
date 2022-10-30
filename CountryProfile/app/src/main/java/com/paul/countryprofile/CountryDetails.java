8406887d2aaf7f6b9b6ff3458b56d753ee84694a
Sourav Paul <35699196+SouravPall@users.noreply.github.com>
2022-10-30 10:46:00 +0600

    get country data

--- CountryProfile/app/src/main/java/com/paul/countryprofile/CountryDetails.java
+++ CountryProfile/app/src/main/java/com/paul/countryprofile/CountryDetails.java
@@ -3,12 +3,45 @@ package com.paul.countryprofile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CountryDetails extends AppCompatActivity {
+    private ImageView imageView;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_country_details);
+
+        imageView = findViewById(R.id.imageview_id);
+        textView = findViewById(R.id.textView_id);
+        
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null){
+            
+            String countryName = bundle.getString("name");
+            
+            showDetails(countryName);
+        }
     }
+
+     void showDetails(String countryName) {
+
+        if(countryName.equals("bangladesh")){
+            imageView.setImageResource(R.drawable.parliament);
+            textView.setText(R.string.bd_text);
+        }
+        if (countryName.equals("U.K")){
+            imageView.setImageResource(R.drawable.uk);
+            textView.setText(R.string.uk_text);
+        }
+        if (countryName.equals("India")){
+            imageView.setImageResource(R.drawable.indiaf);
+            textView.setText(R.string.india_text);
+        }
+    }
+
+
 }
--- CountryProfile/app/src/main/java/com/paul/countryprofile/MainActivity.java
+++ CountryProfile/app/src/main/java/com/paul/countryprofile/MainActivity.java
@@ -2,12 +2,15 @@ package com.paul.countryprofile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button bdButton,ukButton,indiaButton;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +21,29 @@ public class MainActivity extends AppCompatActivity {
         ukButton = findViewById(R.id.buttonUk_id);
         indiaButton = findViewById(R.id.buttonIndia_id);
 
+        bdButton.setOnClickListener(this);
+        ukButton.setOnClickListener(this);
+        indiaButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId()==R.id.buttonBD_id){
+            intent = new Intent(MainActivity.this,CountryDetails.class);
+            intent.putExtra("name","bangladesh");
+            startActivity(intent);
+        }
+        if (v.getId()==R.id.buttonUk_id){
+            intent = new Intent(MainActivity.this,CountryDetails.class);
+            intent.putExtra("name","U.K");
+            startActivity(intent);
+        }
+        if (v.getId()==R.id.buttonIndia_id){
+            intent = new Intent(MainActivity.this,CountryDetails.class);
+            intent.putExtra("name","India");
+            startActivity(intent);
+        }
     }
 }
