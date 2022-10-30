f6bb2d6543a3499b45c82f0dc2a339be9ca7d09d
Sourav Paul <35699196+SouravPall@users.noreply.github.com>
2022-10-30 15:38:27 +0600

    custom adapter get all data

--- CustomAdapter/app/src/main/java/com/paul/customadapter/MainActivity.java
+++ CustomAdapter/app/src/main/java/com/paul/customadapter/MainActivity.java
@@ -10,11 +10,22 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
 
+    private String [] countryName;
+    private int[] flags = {R.drawable.bangladesh,R.drawable.united_states,
+            R.drawable.china,R.drawable.unitedkingdom,R.drawable.germany,
+            R.drawable.canada,R.drawable.france,R.drawable.switzerland,
+            R.drawable.sweden,R.drawable.australia,R.drawable.denmark};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        countryName = getResources().getStringArray(R.array.country_names);
+
         listView = findViewById(R.id.listView_id);
+
+        CustomAdapter adapter = new CustomAdapter(this,countryName,flags);
+        listView.setAdapter(adapter);
     }
 }
