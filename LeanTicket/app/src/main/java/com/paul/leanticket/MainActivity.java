57d58b7e920e69f79947bb3024d6029308113948
Sourav Paul <35699196+SouravPall@users.noreply.github.com>
2022-11-01 19:18:20 +0600

    clean layout ui design

--- LeanTicket/app/src/main/java/com/paul/leanticket/MainActivity.java
+++ LeanTicket/app/src/main/java/com/paul/leanticket/MainActivity.java
@@ -1,13 +1,10 @@
-package com.paul.gridviewdemo;
+package com.paul.leanticket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity{
-
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
