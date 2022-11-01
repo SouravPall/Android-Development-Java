8e1c7b110e002dcee74163255c82ed1184726e22
Sourav Paul <35699196+SouravPall@users.noreply.github.com>
2022-11-01 14:23:17 +0600

    add vector icon

--- OptionMenuDemo/app/src/main/java/com/paul/optionmenudemo/MainActivity.java
+++ OptionMenuDemo/app/src/main/java/com/paul/optionmenudemo/MainActivity.java
@@ -1,10 +1,13 @@
 package com.paul.optionmenudemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +24,24 @@ public class MainActivity extends AppCompatActivity {
         menuInflater.inflate(R.menu.menu_layout,menu);
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.setting_id){
+            Toast.makeText(this, "setting is selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }if(item.getItemId() == R.id.share_id){
+            Toast.makeText(this, "share is selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }if(item.getItemId() == R.id.feedback_id){
+            Toast.makeText(this, "feedback is selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }if(item.getItemId() == R.id.about_id){
+            Toast.makeText(this, "about us is selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
