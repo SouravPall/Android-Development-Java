0d424755c465d58056b1671ca42f386669bec91c
Sourav Paul <35699196+SouravPall@users.noreply.github.com>
2022-11-01 13:51:41 +0600

    search result

--- SearchView/app/src/main/java/com/paul/searchview/MainActivity.java
+++ SearchView/app/src/main/java/com/paul/searchview/MainActivity.java
@@ -3,14 +3,18 @@ package com.paul.searchview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private ListView listView;
     private ArrayAdapter<String> adapter;
-    private String [] countryNames;
+    private SearchView searchView;
+    String [] countryNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +22,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView = findViewById(R.id.listView_id);
+        searchView = findViewById(R.id.searchView_id);
 
         countryNames = getResources().getStringArray(R.array.country_names);
 
-        adapter = new ArrayAdapter<>(MainActivity.this,R.layout)
+        adapter = new ArrayAdapter<>(MainActivity.this,R.layout.sample_view,R.id.countryName_id,countryNames);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 }
