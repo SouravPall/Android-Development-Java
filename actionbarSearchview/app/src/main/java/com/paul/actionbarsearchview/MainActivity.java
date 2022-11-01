e735f30d3ad738d15d21f80632c07c65cce87501
Sourav Paul <35699196+SouravPall@users.noreply.github.com>
2022-11-01 15:58:31 +0600

    action bar searchview

---
+++ actionbarSearchview/app/src/main/java/com/paul/actionbarsearchview/MainActivity.java
@@ -0,0 +1,73 @@
+package com.paul.actionbarsearchview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private String [] questions;
+    private ListView listView;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.setTitle("Question List");
+
+        listView = findViewById(R.id.listView_id);
+
+        questions = getResources().getStringArray(R.array.question_name);
+
+        adapter = new ArrayAdapter<String>(MainActivity.this,R.layout.sample_view,R.id.textView_id, questions);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        String value = adapter.getItem(position);
+
+        Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_layout, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.search_id);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                adapter.getFilter().filter(newText);
+
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
